cb0a541a296073076816166cb11c4462536ed839
georgosjohn <GeorgosJohn@JohnDeere.com>
2020-10-26 14:26:35 -0500

    center brewery container

--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -19,7 +19,7 @@ class BreweryList extends Component{
         return(
             <div className="BreweriesList">
                 <h2 className="BreweryName">{this.props.breweries.name}</h2>
-                <a className="BreweryStreet" href={`http://maps.google.com/?q=${this.props.breweries.street},${this.props.breweries.city},${this.props.breweries.state},${this.props.breweries.postal_code}`} target="_blank">{this.props.breweries.street}</a>
+                <a className="BreweryStreet" href={`http://maps.google.com/?q=${this.props.breweries.street},${this.props.breweries.city},${this.props.breweries.state},${this.props.breweries.postal_code}`} target="_blank">{this.props.breweries.street} </a>
                 <p className="BreweryAddress">{this.props.breweries.city}, {this.props.breweries.state}</p>
                 <p className="BreweryPhone">Phone: {this.props.breweries.phone}</p>
                 Website:<a className="BreweryWebsite" href={this.props.breweries.website_url} target="_blank">{this.props.breweries.name}</a>
