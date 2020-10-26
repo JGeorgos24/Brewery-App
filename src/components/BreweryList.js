b652798960a5fb1715f57690b2f59d3cde302462
georgosjohn <GeorgosJohn@JohnDeere.com>
2020-10-26 14:13:50 -0500

    Add Link to street address to take you to a googles maps for directions

--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -18,12 +18,11 @@ class BreweryList extends Component{
         console.log(this.props.breweries)
         return(
             <div className="BreweriesList">
-                <h2>{this.props.breweries.name}</h2>
-                <p>{this.props.breweries.street}</p>
-                <p>{this.props.breweries.city}, {this.props.breweries.state}</p>
-                <p>Phone: {this.props.breweries.phone}</p>
-                Website:<a href={this.props.breweries.website_url} target="_blank">{this.props.breweries.name}</a>
-                
+                <h2 className="BreweryName">{this.props.breweries.name}</h2>
+                <a className="BreweryStreet" href={`http://maps.google.com/?q=${this.props.breweries.street},${this.props.breweries.city},${this.props.breweries.state},${this.props.breweries.postal_code}`} target="_blank">{this.props.breweries.street}</a>
+                <p className="BreweryAddress">{this.props.breweries.city}, {this.props.breweries.state}</p>
+                <p className="BreweryPhone">Phone: {this.props.breweries.phone}</p>
+                Website:<a className="BreweryWebsite" href={this.props.breweries.website_url} target="_blank">{this.props.breweries.name}</a>
             </div>
         )
     }
